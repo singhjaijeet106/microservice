07f425c9b3b66cfa7b927221a3053ece21092d8d
singhjaijeet106 <155782485+singhjaijeet106@users.noreply.github.com>
2024-07-16 16:11:48 -0400

    Added Config server project

---
+++ config-server/src/test/java/com/example/micro/cfg/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.micro.cfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
